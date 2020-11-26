7c9cff50348c7f1b93b2362036a4353b7f1c6152
Luis Angel <santosangel748@gmail.com>
2020-11-26 17:38:40 -0600

    LocalStorage, haciendo persistente la informacion

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,16 +9,25 @@ export class DeseosService {
   listas: Lista [] = [];
 
   constructor() {
-    const lista1 = new Lista('Recolectar Gemas del Infinito');
-    const lista2 = new Lista('Lugares a visitar');
-
-    this.listas.push(lista1, lista2);
-
+    this.cargarStorage();
   }
 
   crearLista(titulo: string){
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage(){
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage(){
+    if(localStorage.getItem('data')){
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    }else{
+      this.listas = [];
+    }
   }
 
 }
